8c73c775493ba91794413a1050bbb0db2f458534
Marcio Melo <marciomelo@gmail.com>
2018-02-05 21:22:53 -0300

    Trying again

--- bot.js
+++ bot.js
@@ -14,32 +14,40 @@ var retweet = function() {
         result_type: 'recent',
         lang: 'en'
     }
-    Twitter.get('search/tweets', params, function(err, data) {
-      // if there no errors
-        if (!err) {
-          // grab ID of tweet to retweet
-            var retweetId = data.statuses[0].id_str;
-            // Tell TWITTER to retweet
-            Twitter.post('statuses/retweet/:id', {
-                id: retweetId
-            }, function(err, response) {
-                if (response) {
-                    console.log('Retweeted!!!');
-                }
-                // if there was an error while tweeting
-                if (err) {
-                    console.log('Something went wrong while RETWEETING... Duplication maybe...');
-                }
-            });
-        }
-        // if unable to Search a tweet
-        else {
-          console.log('Something went wrong while SEARCHING...');
-        }
-    });
+
+    Twitter.get('search/tweets', { q: 'varacast', count: 1 }, function(err, data, response) {
+  		console.log(data)
+	});
+    // Twitter.get('search/tweets', params, function(err, data, response) {
+    //   // if there no errors
+    //     if (!err) {
+    //     	console.log(data);
+
+    //       // grab ID of tweet to retweet
+    //         var retweetId = data.statuses[0].id_str;
+    //         // Tell TWITTER to retweet
+    //         Twitter.post('statuses/retweet/:id', {
+    //             id: retweetId
+    //         }, function(err, response) {
+    //             if (response) {
+    //                 console.log('Retweeted!!!');
+    //             }
+    //             // if there was an error while tweeting
+    //             if (err) {
+    //                 console.log('Something went wrong while RETWEETING... Duplication maybe...');
+    //             }
+    //         });
+    //     }
+    //     // if unable to Search a tweet
+    //     else {
+    //       console.log('Something went wrong while SEARCHING...');
+    //     }
+    // });
 }
 
 // grab & retweet as soon as program is running...
 retweet();
 // retweet in every 50 minutes
 setInterval(retweet, 3000000);
+
+//Se this => http://techknights.org/workshops/nodejs-twitterbot/
