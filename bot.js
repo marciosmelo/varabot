ec520007dffe1932e9a5018d9bab5ed70a1e0db4
@marciosmelo <marciomelo@gmail.com>
2018-02-06 22:01:50 -0300

    Add the favorite function

--- bot.js
+++ bot.js
@@ -4,16 +4,15 @@ var twit = require('twit'),
 
 var Twitter = new twit(config);
 
+var params = {
+    q: 'varacast',  // REQUIRED
+    result_type: 'recent'
+}
+
 // RETWEET BOT ==========================
 
 // find latest tweet according the query 'q' in params
 var retweet = function() {
-    var params = {
-        q: 'varacast',  // REQUIRED
-        result_type: 'mixed',
-	count: 1    
-    }
-
     Twitter.get('search/tweets', params, function(err, data, response) {
   		if (!err) {
  			var retweetId = data.statuses[0].id_str;
@@ -39,5 +38,43 @@ retweet();
 // retweet in every 50 minutes
 setInterval(retweet, 3000000);
 
+// FAVORITE BOT====================
 
+// find a random tweet and 'favorite' it
+var favoriteTweet = function(){
+    // find the tweet
+    Twitter.get('search/tweets', params, function(err,data,response){
+        if (!err) {
+        // find tweets
+            var tweet = data.statuses;
+            var randomTweet = ranDom(tweet);   // pick a random tweet
+                // if random tweet exists
+                if(typeof randomTweet != 'undefined'){
+                    // Tell TWITTER to 'favorite'
+                    Twitter.post('favorites/create', {id: randomTweet.id_str}, function(err, response){
+                        // if there was an error while 'favorite'
+                        if(err){
+                            console.log('CANNOT BE FAVORITE... Error');
+                        }
+                        else{
+                            console.log('FAVORITED... Success!!!');
+                        }
+                    });
+                }else{
+                    console.log(err, "Error at favorites/tweets function");
+                }
+       }
+    });
+  }
+  // grab & 'favorite' as soon as program is running...
+  favoriteTweet();
+  // 'favorite' a tweet in every 60 minutes
+  setInterval(favoriteTweet, 3600000);
+  
+  // function to generate a random tweet tweet
+  function ranDom (arr) {
+    var index = Math.floor(Math.random()*arr.length);
+    return arr[index];
+  };
 
+ // for more parametes, see: https://dev.twitter.com/rest/reference
