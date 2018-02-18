5589f923aa72a6aea0cdfea8c5573b3116107dcf
Marcio Melo <marciomelo@gmail.com>
2018-02-18 10:22:57 -0300

    Adding the replies with randomic messages

--- bot.js
+++ bot.js
@@ -10,6 +10,61 @@ var params = {
     count: 20
 }
 
+// we will randomly pick one of these items in this array
+var arrResponses = [
+    "Calma porcouvinte, Vara é apenas o coletivo de porcos.",
+    "Oinc, oinc!",
+    "Como já diria @Wildpoxx: F***** o Brasil! METAAAAALLLL!!!",
+    "Sem dúvidas.",
+    "Meu porcosentido diz que não.",
+    "Ouça nosso último podcast e vais entender melhor.",
+    "Na POCILGA.com.br temos o melhor time de porcolunistas.",
+    "Respire, concentre-se, e tente outra vez.",
+    "Sei não.",
+    "Gelo fino, gelo fino....",
+    "Porco-aranha, porco-aranha.....",
+    "Dúvido bastante.",
+    "Sim, claro.",
+    "SOOOOOOOOOOBE WAR PIGS!!!.",
+    "TARAAAN.... TAN TAN TAAAAAN TANRAAAANNNN!!!",
+    "Diga oláaaaa...",
+    "Pergunte-me outra vez, mais tarde.",
+    "Minha resposta pode ser não.",
+    "Será?",
+    "Não conte com isto."
+  ]
+
+//The @Varacast timeline
+var stream = Twitter.stream('statuses/filter', {track: 'varacast'}); 
+
+stream.on('tweet', function(tweet){
+    console.log(tweet.text);
+  
+    // calculate the random index (Math.random returns a double between 0 and 1)
+    var randomIndex = Math.round(Math.random() * arrResponses.length);
+
+    //build our reply string grabbing the string in that randomIndex we've calculated
+    var reply = "Olá @" + tweet.user.screen_name + ", " + arrResponses[randomIndex];
+
+    //call the post function to tweet something
+    Twitter.post('statuses/update', {status: reply},  function(error, tweetReply, response){
+  
+        //if we get an error print it out
+        if(error){
+          console.log(error);
+        }
+  
+        //print the text of the tweet we sent out
+        console.log(tweetReply.text);
+      });
+
+});
+
+stream.on('error', function(error) {
+    //print out the error
+    console.log(error);
+});
+
 // RETWEET BOT ==========================
 
 // find latest tweet according the query 'q' in params
@@ -65,9 +120,9 @@ var favoriteTweet = function(){
 
 
   // grab & retweet as soon as program is running...
-  retweet();
+  //retweet();
   // grab & 'favorite' as soon as program is running...
-  favoriteTweet();
+  //favoriteTweet();
   // retweet in every 50 minutes
   setInterval(retweet, 3600000);
   // 'favorite' a tweet in every 30 minutes
