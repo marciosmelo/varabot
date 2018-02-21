d73944bf95620bb8fa5f6b5084d1e4dcd0f9607f
@marciosmelo <marciomelo@gmail.com>
2018-02-20 22:10:58 -0300

    Reverting changes

--- bot.js
+++ bot.js
@@ -6,98 +6,50 @@ var Twitter = new twit(config);
 
 var params = {
     q: 'varacast',  // REQUIRED
-    result_type: 'mixed',
-    count: 20
+    result_type: 'recent'
 }
-
-// we will randomly pick one of these items in this array
-var arrResponses = [
-    "Calma porcouvinte, Vara é apenas o coletivo de porcos.",
-    "Oinc, oinc!",
-    "Como já diria @Wildpoxx: F***** o Brasil! METAAAAALLLL!!!",
-    "Sem dúvidas.",
-    "Meu porcosentido diz que não.",
-    "Ouça nosso último podcast e vais entender melhor.",
-    "Na POCILGA.com.br temos o melhor time de porcolunistas.",
-    "Respire, concentre-se, e tente outra vez.",
-    "Sei não.",
-    "Gelo fino, gelo fino....",
-    "Porco-aranha, porco-aranha.....",
-    "Dúvido bastante.",
-    "Sim, claro.",
-    "SOOOOOOOOOOBE WAR PIGS!!!.",
-    "TANRAAAN.... TAN TAN TAAAAAN TANRAAAANNNN!!!",
-    "Diga oláaaaa...",
-    "Pergunte-me outra vez, mais tarde.",
-    "Minha resposta pode ser não.",
-    "Será?",
-    "Não conte com isto."
-  ]
-
-//The @Varacast timeline
-var stream = Twitter.stream('statuses/filter', {track: 'varacast'}); 
-
-stream.on('tweet', function(tweet){
-    console.log(tweet.text);
-  
-    var username = tweet.user.screen_name;
-    if (username != 'varacast') {
-        var reply = "Olá @" + username + ", " + ranDomItemFromArray(arrResponses);
-        //call the post function to tweet something
-        Twitter.post('statuses/update', {status: reply},  function(error, tweetReply, response){
-    
-            //if we get an error print it out
-            if(error){
-            console.log(error);
-            }
-    
-            //print the text of the tweet we sent out
-            console.log(tweetReply.text);
-        });
-    }
-});
-
-stream.on('error', function(error) {
-    //print out the error
-    console.log(error);
-});
 
 // RETWEET BOT ==========================
 
 // find latest tweet according the query 'q' in params
-var retweet = () => {
-    var retweet = getTweetFromSearch(params);
-    if (null !== retweet){
-        Twitter.post('statuses/retweet/:id', {
-            id: retweet.id_str
-        },function(err, response) {
-            if (response) {
-                console.log('Retweeted!!!');
-            }
-            if (err) {
-                console.log('Something went wrong while RETWEETING: ' + err);
-            }
-        });
-    }
+var retweet = function() {
+    Twitter.get('search/tweets', params, function(err, data, response) {
+  		if (!err) {
+ 			var retweetId = data.statuses[0].id_str;
+  			 Twitter.post('statuses/retweet/:id', {
+                 id: retweetId
+             },function(err, response) {
+                if (response) {
+                    console.log('Retweeted!!!');
+                }
+                 if (err) {
+                    console.log('Something went wrong while RETWEETING... Duplication maybe...');
+                }
+            });
+  		}else{
+  			console.log(err, "Error at search/tweets function");
+  		}
+	});
+   
 }
 
 // FAVORITE BOT====================
 
 // find a random tweet and 'favorite' it
-var favoriteTweet = () => {
+var favoriteTweet = function(){
     // find the tweet
     Twitter.get('search/tweets', params, function(err,data,response){
         if (!err) {
         // find tweets
             var tweet = data.statuses;
-            var randomTweet = ranDomItemFromArray(tweet);   // pick a random tweet
+            var randomTweet = ranDom(tweet);   // pick a random tweet
                 // if random tweet exists
                 if(typeof randomTweet != 'undefined'){
                     // Tell TWITTER to 'favorite'
                     Twitter.post('favorites/create', {id: randomTweet.id_str}, function(err, response){
                         // if there was an error while 'favorite'
                         if(err){
-                            console.log('CANNOT BE FAVORITE... Error: ' + err);
+                            console.log('CANNOT BE FAVORITE... Error');
                         }
                         else{
                             console.log('FAVORITED... Success!!!');
@@ -110,29 +62,18 @@ var favoriteTweet = () => {
     });
   }
 
-  //Run as soon as the app starts
+  // grab & retweet as soon as program is running...
   retweet();
+  // grab & 'favorite' as soon as program is running...
   favoriteTweet();
-  //Every 60 min RT | 30 min Fave
-  setInterval(retweet, 3600000);
-  setInterval(favoriteTweet, 1800000);
-
-  function ranDomItemFromArray (arr) {
+  // retweet in every 50 minutes
+  setInterval(retweet, 3000000);
+  // 'favorite' a tweet in every 60 minutes
+  setInterval(favoriteTweet, 3600000);
+  // function to generate a random tweet tweet
+  function ranDom (arr) {
     var index = Math.floor(Math.random()*arr.length);
     return arr[index];
   };
 
-  function getTweetFromSearch(searchParams){
-    Twitter.get('search/tweets', searchParams, function(err, data, response) {
-        if (!err) {
-           return ranDomItemFromArray(data.statuses);         
-        }else{
-            console.log(err, "Error at search/tweets function");
-            return null;
-        }
-  });
-
-  }
-
  // for more parametes, see: https://dev.twitter.com/rest/reference
-
