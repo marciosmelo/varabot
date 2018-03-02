3be7884ab06f8f3428bcffac158fc961410b10db
Marcio Melo <marciomelo@gmail.com>
2018-03-02 17:33:42 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,7 +40,7 @@ stream.on('tweet', function(tweet){
   
     var username = tweet.user.screen_name;
     if (username != 'varacast') {
-        var reply = "Olá @" + username + ". " + ranDom(arrResponses);
+        var reply = "Olá @" + username + "... " + ranDom(arrResponses);
         //call the post function to tweet something
         Twitter.post('statuses/update', {status: reply},  function(error, tweetReply, response){
     
