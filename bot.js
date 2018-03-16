f6be68fdc6ebba8f5f85001f867bd6f998c51353
Marcio Melo <marciomelo@gmail.com>
2018-03-16 08:11:25 -0300

    Refactoring

--- bot.js
+++ bot.js
@@ -9,56 +9,6 @@ var params = {
     count: 20
 }
 
-var arrResponses = [
-    "Calma porcouvinte, vara é apenas o coletivo de porcos.",
-    "Oinc, oinc!",
-    "Como já diria @Wildpoxx: F***** o Brasil! METAAAAALLLL!!!",
-    "Sem dúvidas.",
-    "Meu porcosentido diz que não.",
-    "Ouça nosso último podcast e vais entender melhor.",
-    "Na POCILGA.com.br temos o melhor time de porcolunistas.",
-    "Respire, concentre-se, e tente outra vez.",
-    "Sei não.",
-    "Gelo fino, gelo fino....",
-    "Porco-aranha, porco-aranha.....",
-    "Duvido bastante.",
-    "Sim, claro.",
-    "SOOOOOOOOOOBE WAR PIGS!!!.",
-    "TANRAAAN.... TAN TAN TAAAAAN TANRAAAANNNN!!!",
-    "Diga oláaaaa...",
-    "Pergunte-me outra vez, mais tarde.",
-    "Minha resposta pode ser não.",
-    "Será?",
-    "Não conte com isto."
-]
-
-//The @Varacast timeline
-var stream = Twitter.stream('statuses/filter', {track: 'varacast'}); 
-
-stream.on('tweet', function(tweet){
-    console.log(tweet.text);
-  
-    var username = tweet.user.screen_name;
-    if (username != 'varacast') {
-        var reply = "Olá @" + username + "... " + ranDom(arrResponses);
-        //call the post function to tweet something
-        Twitter.post('statuses/update', {status: reply},  function(error, tweetReply, response){
-    
-            if(error){
-                console.log(error);
-            }else{    
-                console.log(tweetReply.text);
-            }
-        });
-    }
-});
-
-stream.on('error', function(error) {
-    console.log(error);
-});
-
-
-
 // BOT Search for Fave and RT
 var tweetBot = () => {
     // find the tweet
@@ -75,12 +25,10 @@ var tweetBot = () => {
                     if (user != 'varacast'){
                         faveTweet(randomTweet);
                         retweet(randomTweet);
-                        //replyTweetWithRandonAnswer(randomTweet);
                     }
                 }catch(ex){
                     console.log(ex);
-                }
-                
+                }                
             }else{
                 console.log(err, "Error at favorites/tweets function");
             }
@@ -109,21 +57,6 @@ function retweet(tweet){
            console.log('Something went wrong while RETWEETING: ' + err);
         }
     });
-}
-
-function replyTweetWithRandonAnswer(tweet){
-    var username = tweet.user.screen_name;
-    console.log('username: ' + username);
-    if (username != 'varacast') {
-        var reply = "Olá @" + username + ", " + ranDom(arrResponses);
-        Twitter.post('statuses/update', {status: reply},  function(error, tweetReply, response){
-            if(error){
-                console.log(error);
-            }else{    
-                console.log(tweetReply.text);
-            }
-        });
-    }
 }
 
 function ranDom (arr) {
